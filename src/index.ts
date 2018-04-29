6cbac52332e8f4a11b5498bb9fbccc46cbac9310
rob kurtz <rob@robkurtz.net>
2018-04-29 15:33:01 -0600

    end of part 1 - broken

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
 import 'reflect-metadata';
-
 import { GraphQLServer } from 'graphql-yoga';
+import { importSchema } from 'graphql-import';
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
+import { resolvers } from './resolvers';
 
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Bye ${name || 'World'}`
-  }
-};
+const typeDefs = importSchema('schema.graphql');
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => console.log('Server is running on localhost:4000'));
