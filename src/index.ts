7f9333f69ba50163d963e56008a7244064da5f7f
rob kurtz <rob@robkurtz.net>
2018-04-30 10:50:28 -0600

    end of part 5 - graphql yoga and jest

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
 import 'reflect-metadata';
-import { GraphQLServer } from 'graphql-yoga';
-import { importSchema } from 'graphql-import';
-import { createConnection } from 'typeorm';
-import * as path from 'path';
+import { startServer } from './startServer';
 
-import { resolvers } from './resolvers';
-
-const typeDefs = importSchema(path.join(__dirname, './schema.graphql'));
-
-const server = new GraphQLServer({ typeDefs, resolvers });
-createConnection().then(() => {
-  server.start(() => console.log('Server is running on localhost:4000'));
-});
+startServer();
--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,8 +1,16 @@
 import { request } from 'graphql-request';
-import { createConnection } from 'typeorm';
 
-import { host } from './constants';
 import { User } from '../entity/User';
+import { startServer } from '../startServer';
+
+// TODO what happens if we do this as a string as opposed to a function?
+let getHost = () => '';
+
+beforeAll(async () => {
+  const app = await startServer();
+  const { port } = app.address();
+  getHost = () => `http://127.0.0.1:${port}`;
+});
 
 const email = 'tom@bob.com';
 const password = 'jalksdf';
@@ -14,16 +22,11 @@ mutation {
 `;
 
 test('Register user', async () => {
-  const response = await request(host, mutation);
+  const response = await request(getHost(), mutation);
   expect(response).toEqual({ register: true });
-  await createConnection();
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
   const user = users[0];
   expect(user.email).toEqual(email);
   expect(user.password).not.toEqual(password);
 });
-
-// use a test database
-// drop all data once the test is over
-// when I run yarn test it also starts the server
